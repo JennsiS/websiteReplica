7287441b5be3b6629a701f775cd478ccf4981f8b
Jennifer Sandoval <san18962@uvg.edu.gt>
2022-04-13 22:27:39 -0600

    Adding images and initial components

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import moonImg from '../img/moon-large.jpg';
+
+export default function App() {
+  return (
+    <div className="section--header">
+      <h1 className="moontopia-title">MOONTOPIA</h1>
+      <img src={moonImg} alt="" />
+    </div>
+  );
+
+}
